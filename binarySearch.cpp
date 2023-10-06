285d2d709d862fca016f1b69befdcb2210e402ba
Admin <nguyenhungthinha1@gmail.com>
2023-10-06 22:07:00 +0700

    Add binary Seach function

---
+++ binarySearch.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int binarySearch(int A[], int n, int x) {
+    int start = 0;
+    int end = n - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        
+        if (A[mid] == x) {
+            return mid;
+        }
+
+        else if (A[mid] > x)
+            end = mid - 1;
+        else
+            start = mid + 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    int a[] = {1, 3, 5, 7, 9, 10, 35, 124};
+    // cout << binarySearch(a, sizeof(a) / sizeof(int), 10);
+    int x = 5;
+    int y = 2;
+    cout << x / y*1.0 << endl;
+
+    return 0;
+}
